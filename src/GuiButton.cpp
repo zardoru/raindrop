3ab22db5ff1cbe5b05a58a4bab4b1e2522c58388
zardoru <forsakengmex@gmail.com>
2013-10-25 12:29:11 -0300

    Missing files + README update

---
+++ src/GuiButton.cpp
@@ -0,0 +1,40 @@
+#include "Global.h"
+#include "GameWindow.h"
+#include "GuiButton.h"
+
+using namespace GUI;
+
+Button::Button()
+{
+	OnHover = OnClick = OnLeave = NULL;
+	PressedDown = false;
+}
+
+bool Button::HandleInput(int32 key, KeyEventType code, bool isMouseInput)
+{
+	if (isMouseInput && BindingsManager::TranslateKey(key) == KT_Select)
+	{
+		glm::vec2 mpos = WindowFrame.GetRelativeMPos();
+		glm::vec2 bpos = GetPosition();
+		if (mpos.x > bpos.x && mpos.x < bpos.x+GetWidth())
+		{
+			if (mpos.y > bpos.y && mpos.y < bpos.y+GetHeight())
+			{
+				if (code == KE_Press)
+				{
+					if (OnClick) OnClick;
+					PressedDown = true;
+				}
+			}
+		}
+
+		if (code == KE_Release && PressedDown)
+		{
+			if (OnRelease) OnRelease;
+			PressedDown = false;
+			return true;
+		}
+	}
+
+	return false;
+}
