fe0c8c99970bfec5d907a72f0e0956abe861250f
zardoru <forsakengmex@gmail.com>
2014-09-14 16:43:22 -0300

    Fix L9999999999999^99999999999 with proper loader bpm behaviour

--- src/NoteLoaderBMS.cpp
+++ src/NoteLoaderBMS.cpp
@@ -284,7 +284,10 @@ void CalculateBPMs(BmsLoadInfo *Info)
 			for (BMSEventList::iterator ev = i->second.Events[CHANNEL_BPM].begin(); ev != i->second.Events[CHANNEL_BPM].end(); ev++)
 			{
 				double BPM;
-				BPM = fromBase16(tob36(ev->Event).c_str());
+				if (Info->BPMs.find(ev->Event) != Info->BPMs.end())
+					BPM = Info->BPMs[ev->Event];
+				else 
+					BPM = fromBase16(tob36(ev->Event).c_str());
 
 				double Beat = ev->Fraction * 4 * i->second.BeatDuration + BeatForMeasure(Info, i->first);
 
