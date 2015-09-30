dd458c9fedc0499584cbf750dff26681f0f54fa1
zardoru <forsakengmex@gmail.com>
2015-09-30 15:21:48 -0300

    Make random respect scratch.
    Also fix that it can only swap earlier indices than the current one, not
    later ones.

--- src/NoteTransformations.cpp
+++ src/NoteTransformations.cpp
@@ -13,27 +13,27 @@ namespace NoteTransform {
 		if (RespectScratch)
 			si = 1; else si = 0;
 
-		for (auto k = si; k < ChannelCount; k++)
-		{
-			chan_index.push_back(k);
-		}
-
 		std::mt19937 dev;
 		dev.seed(time(nullptr));
 		std::uniform_int_distribution<int> di(si, ChannelCount - 1);
 
-		std::random_shuffle(chan_index.begin() + si, chan_index.end(), [&](int i) -> int { return di(dev); });
+		std::random_shuffle(Notes + si, Notes + ChannelCount, [&](int i) -> int {
+			int rv = -1;
+			while (rv < si)
+				rv = di(dev) % i; // Only can swap with earlier element, and it's the i-th position, not index, so let's % it.
+			return rv;
+		});
 
-		for (auto k = si; k < ChannelCount; k++)
-			swap(Notes[k], Notes[chan_index[k]]);
+		for (auto k = si; k < chan_index.size(); k++)
+			swap(Notes[k], Notes[chan_index[k-1]]);
 	}
 
 	void Mirror(VSRG::VectorTN &Notes, int ChannelCount, bool RespectScratch)
 	{
 		int k;
 		if (RespectScratch) k = 1; else k = 0;
-		for (; k < ChannelCount; k++)
-			swap(Notes[k], Notes[ChannelCount - 1]);
+		for (int v = ChannelCount - 1 ; k < ChannelCount / 2; k++, v--)
+			swap(Notes[k], Notes[ChannelCount - k]);
 	}
 
 }
