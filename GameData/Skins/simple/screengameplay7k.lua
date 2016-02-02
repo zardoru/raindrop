c6f72d6a6b575199279057f5fe037770febfc32c
zardoru <forsakengmex@gmail.com>
2016-02-02 18:58:38 -0300

    Add fail/succeed animations to simple (#27)

--- GameData/Skins/simple/screengameplay7k.lua
+++ GameData/Skins/simple/screengameplay7k.lua
@@ -1,20 +1,22 @@
 if Channels ~= 8 then
-	fallback_require("screengameplay7k")
+	fallback_require "screengameplay7k"
 	return
 end
 
-game_require("TextureAtlas")
-game_require("FixedObjects")
+game_require "TextureAtlas"
+game_require "FixedObjects"
 
-skin_require("custom_defs")
+skin_require "custom_defs"
 
-game_require("utils")
-game_require("AnimationFunctions")
+game_require "utils"
+game_require "AnimationFunctions"
 
-skin_require("Global/FadeInScreen")
+skin_require "Global/FadeInScreen"
+skin_require "VSRG/StageAnimation"
 -- Set up constants for everyone
 
 
+-- Scale from 1280x720
 YR = 768 / 720
 XR = 1360 / 1280
 
@@ -80,19 +82,19 @@ function GenText()
 	BPMText.Y = 660 * YR
 	BPMText.Font = NumFont
 	BPMText.Layer = 21
-	
+
 	HPText = StringObject2D()
 	HPText.X = 315 * XR
 	HPText.Y = 605 * YR
 	HPText.Font = NumFont
 	HPText.Layer = 21
-	
+
 	TitleText = StringObject2D()
 	TitleText.X = 455 * XR
 	TitleText.Y = 5 * YR
 	TitleText.Font = TitleFont
 	TitleText.Layer = 21
-	
+
 	sng = toSong7K(Global:GetSelectedSong())
 	diff = sng:GetDifficulty(Global.DifficultyIndex)
 	if diff.Author ~= "" then
@@ -101,7 +103,7 @@ function GenText()
 		difftxt = string.format("%s", diff.Name)
 	end
 	TitleText.Text = string.format("%s\n%s\n%s", sng.Title, sng.Author, difftxt)
-	
+
 	Engine:AddTarget(BPMText)
 	Engine:AddTarget(HPText)
 	Engine:AddTarget(TitleText)
@@ -110,48 +112,54 @@ end
 function Init()
 	FixedObjects.XRatio = XR
 	FixedObjects.YRatio = YR
-	AutoadjustBackground({x = 540 * FixedObjects.XRatio, y = 84 * FixedObjects.YRatio, 
-						  w = 640 * FixedObjects.XRatio, h = 480 * FixedObjects.YRatio})
+	AutoadjustBackground({
+		x = 540 * XR,
+		y = 84 * YR,
+		w = 640 * XR,
+		h = 480 * YR
+	})
+
 	ScreenFade.Init()
 	ScreenFade.Out(true)
-	
+
 	ComboDisplay.Init()
 	Judgment.Init()
 	ScoreDisplay.Init()
-	
+	IsFullCombo = false
+
 	GenText()
 	print "Creating fixed objects."
 	FixedObjects.CreateFromCSV("simple.csv", Noteskin[8])
-	
+
 	SongPosition = Sprites["tick"]
 	SongPosition.Centered = true
-	
+
 	tabletick = Sprites["tt"]
 	tabletick.Centered = true
-	
+
 	for i=1, Channels do
 		if i < 8 then
 			Key[i] = Sprites["l" .. i]
-			
+
 			Key[i].Alpha = 0
 			Key[i].BlendMode = BlendAdd
 		end
-		
+
 		Lightning[i] = {}
-		
+
 		Lightning[i].CurrentTime = 0
-		
+
 		Lightning[i].Object = Sprites["beam_k" .. i]
 		Lightning[i].Object.BlendMode = BlendAdd
 		Lightning[i].Object.Alpha = 1
 		Lightning[i].Object.Centered = 1
 		Lightning[i].Object.Y = Lightning[i].Object.Y + Lightning[i].Object.Height / 2
-		
+
 		Lightning[i].Update = function(self, delta)
 			self.Object.ScaleX = 1 - math.pow(1 - self.CurrentTime / LightingTime, 0.75)
 			self.CurrentTime = max(self.CurrentTime - delta, 0)
 		end
-			
+
 		Bomb[i] = {}
 		Bomb[i].Object = Sprites["bomb_k" .. i]
 		Bomb[i].Object.Alpha = 0
@@ -159,7 +167,7 @@ function Init()
 		Bomb[i].CurrentTime = 0
 		Bomb[i].Update = function(self, delta)
 			local lerp = self.CurrentTime / BombTime
-			if lerp > 0.5 then 
+			if lerp > 0.5 then
 				self.Object.Alpha = lerp
 			else
 				self.Object.Alpha = 2 * lerp
@@ -169,12 +177,12 @@ function Init()
 			self.CurrentTime = max(self.CurrentTime - delta, 0)
 		end
 	end
-	
+
 	HP = Sprites["health"]
 	HP.ScaleX = LifebarValue
 	HP:SetCropByPixels(0, 352 * LifebarValue, 0, 29)
 	Engine:Sort()
-	
+
 	Pulse = Sprites["pulse"]
 	Pulse.Y = 485 * YR - Pulse.Height
 	Pulse.Lighten = 1
@@ -185,10 +193,12 @@ function Cleanup()
 end
 
 function OnFullComboEvent()
+	IsFullCombo = true
 end
 
 function OnFailureEvent()
-	return 0
+	DoFailAnimation()
+	return FailAnimation.Duration
 end
 
 -- When 'enter' is pressed and the game starts, this function is called.
@@ -197,12 +207,12 @@ end
 
 function HitEvent(JudgmentValue, TimeOff, Lane, IsHold, IsHoldRelease)
     local MapLane = Noteskin[Channels].Map[Lane]
-	
+
 	local eol = 0
 	if TimeOff > 0 then eol = 2 elseif TimeOff < 0 then eol = 1 end
 	Judgment.Hit(JudgmentValue, eol)
 	ComboDisplay.Hit(JudgmentValue == 0)
-	
+
 	Bomb[MapLane].CurrentTime = BombTime
 	ScoreDisplay.Update()
 end
@@ -211,7 +221,7 @@ function MissEvent(TimeOff, Lane, IsHold)
 	local eol = 0
 	if TimeOff < 0 then eol = 2 elseif TimeOff > 0 then eol = 1 end
 	Judgment.Hit(5, eol)
-	
+
 	ComboDisplay.Hit(0)
 end
 
@@ -221,12 +231,12 @@ end
 function GearKeyEvent (Lane, IsKeyDown)
     local MapLane = Noteskin[Channels].Map[Lane + 1]
 	KeyArray[MapLane] = IsKeyDown
-	
+
 	Lightning[MapLane].Object.Alpha = 1
 	Lightning[MapLane].CurrentTime = LightingTime
 
 	if MapLane > 1 then
-		if IsKeyDown == 1 then 
+		if IsKeyDown == 1 then
 			Key[MapLane - 1].Alpha = 1
 		else
 			Key[MapLane - 1].Alpha = 0
@@ -236,50 +246,50 @@ end
 
 -- Called when the song is over.
 function OnSongFinishedEvent()
-	return 0
+	DoSuccessAnimation()
+	return SuccessAnimation.Duration
 end
 
 function Update(Delta)
 	-- Executed every frame.
 	local beatEffect = Beat - math.floor(Beat)
-	
+
 	local SongPercentage = Game:GetSongTime() / (SongDuration + 3)
-	
+
 	if Game:GetSongTime() < 0 then
 		SongPercentage = math.pow(SongTime / -1.5, 2)
 	end
-	
+
 	SongPosition.Y = 62 + 383 * SongPercentage * XR
-	
+
 	HP.ScaleX = math.ceil(LifebarValue * 50) / 50
 	HP:SetCropByPixels(0, 352 * math.ceil(LifebarValue * 50) / 50, 0, 29)
-	
+
 	Pulse.Alpha = 1 - clamp(beatEffect, 0.5, 1)
 	Pulse.LightenFactor = 1 - beatEffect
-	
+
 	if KeyArray[1] == 0 then
 		tabletick.Rotation = Beat * 360
 	else
 		tabletick.Rotation = - Beat * 360
 	end
-	
+
 	for i=1,Channels do
 		if KeyArray[i] == 1 then
 			Lightning[i].CurrentTime = LightingTime
 		end
-		
+
 		Lightning[i]:Update(Delta)
 		Bomb[i]:Update(Delta)
 	end
-	
-	if CurrentBPM ~= 0 then 
+
+	if CurrentBPM ~= 0 then
 		BPMText.Text = string.format("%03d", CurrentBPM)
 	end
-	
+
 	HPText.Text = string.format("%03d%%", LifebarDisplay)
-	
+
 	Judgment.Run(Delta)
 	ComboDisplay.Run(Delta)
 	ScoreDisplay.Run(Delta)
 end
-
