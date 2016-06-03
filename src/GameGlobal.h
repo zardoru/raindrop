a5b235c70560961760136350256ef1c055bd14db
zardoru <forsakengmex@gmail.com>
2016-06-03 14:39:21 -0400

    Version increase.

--- src/GameGlobal.h
+++ src/GameGlobal.h
@@ -211,7 +211,7 @@ const float MeasureBaseSpacing = 0.8f * ScreenHeightDefault;
 
 /* Program itself consts */
 #define RAINDROP_WINDOWTITLE "raindrop ver: "
-#define RAINDROP_VERSION "0.300"
+#define RAINDROP_VERSION "0.400"
 #ifdef NDEBUG
 #define RAINDROP_BUILDTYPE " "
 #else
