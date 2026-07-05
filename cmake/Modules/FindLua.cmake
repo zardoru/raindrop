bcd416886d4b6e224a332fac7cf8b5cd084af95a
zardoru <diao.lzr@gmail.com>
2026-07-05 03:24:08 -0400

    replacing more vendoring for fetch scripts

--- cmake/Modules/FindLua.cmake
+++ cmake/Modules/FindLua.cmake
@@ -1,7 +1,5 @@
 include(FindPackageHandleStandardArgs)
-include(ExternalProject)
-
-find_program(MAKE_EXECUTABLE NAMES gmake make REQUIRED)
+include(FetchContent)
 
 find_path(LUA_INCLUDE_DIR lua.h
     PATH_SUFFIXES lua lua5.4 lua54 lua5.3 lua53
@@ -17,14 +15,25 @@ if(LUA_INCLUDE_DIR AND LUA_LIBRARY AND NOT TARGET Lua::Lua)
 endif()
 
 if(NOT TARGET Lua::Lua)
-    set(LUA_EXTERNAL_PREFIX "${CMAKE_BINARY_DIR}/_deps/lua")
-    set(LUA_EXTERNAL_INSTALL_DIR "${LUA_EXTERNAL_PREFIX}/install")
-    set(LUA_EXTERNAL_INCLUDE_DIR "${LUA_EXTERNAL_INSTALL_DIR}/include")
-    set(LUA_EXTERNAL_LIBRARY "${LUA_EXTERNAL_INSTALL_DIR}/lib/liblua.a")
-    set(LUA_EXTERNAL_HPP "${LUA_EXTERNAL_PREFIX}/lua.hpp")
+    FetchContent_Declare(lua
+        GIT_REPOSITORY https://github.com/lua/lua.git
+        GIT_TAG v5.4.8
+        GIT_SHALLOW TRUE
+        UPDATE_DISCONNECTED TRUE
+    )
 
-    file(MAKE_DIRECTORY "${LUA_EXTERNAL_INCLUDE_DIR}")
-    file(WRITE "${LUA_EXTERNAL_HPP}" [=[
+    FetchContent_MakeAvailable(lua)
+
+    set(LUA_FETCH_INCLUDE_DIR "${CMAKE_BINARY_DIR}/_deps/lua/include")
+    file(MAKE_DIRECTORY "${LUA_FETCH_INCLUDE_DIR}")
+    file(COPY
+        "${lua_SOURCE_DIR}/lua.h"
+        "${lua_SOURCE_DIR}/luaconf.h"
+        "${lua_SOURCE_DIR}/lualib.h"
+        "${lua_SOURCE_DIR}/lauxlib.h"
+        DESTINATION "${LUA_FETCH_INCLUDE_DIR}"
+    )
+    file(WRITE "${LUA_FETCH_INCLUDE_DIR}/lua.hpp" [=[
 #pragma once
 
 extern "C" {
@@ -34,51 +43,23 @@ extern "C" {
 }
 ]=])
 
+    add_library(lua_onelua STATIC "${lua_SOURCE_DIR}/onelua.c")
+    target_compile_definitions(lua_onelua PRIVATE MAKE_LIB)
+    target_include_directories(lua_onelua PUBLIC "$<BUILD_INTERFACE:${LUA_FETCH_INCLUDE_DIR}>")
+
     if(APPLE)
-        set(LUA_EXTERNAL_CFLAGS "-DLUA_USE_MACOSX -fPIC")
+        target_compile_definitions(lua_onelua PRIVATE LUA_USE_MACOSX)
     elseif(UNIX)
-        set(LUA_EXTERNAL_CFLAGS "-DLUA_USE_LINUX -fPIC")
-    else()
-        set(LUA_EXTERNAL_CFLAGS "-fPIC")
+        target_compile_definitions(lua_onelua PRIVATE LUA_USE_LINUX)
+        target_link_libraries(lua_onelua PUBLIC m ${CMAKE_DL_LIBS})
     endif()
 
-    ExternalProject_Add(lua_external
-        PREFIX "${LUA_EXTERNAL_PREFIX}"
-        GIT_REPOSITORY https://github.com/lua/lua.git
-        GIT_TAG v5.4.8
-        GIT_SHALLOW TRUE
-        UPDATE_COMMAND ""
-        UPDATE_DISCONNECTED TRUE
-        CONFIGURE_COMMAND ""
-        BUILD_IN_SOURCE TRUE
-        BUILD_BYPRODUCTS "${LUA_EXTERNAL_LIBRARY}"
-        BUILD_COMMAND
-            "${MAKE_EXECUTABLE}"
-            "CC=${CMAKE_C_COMPILER}"
-            "MYCFLAGS=${LUA_EXTERNAL_CFLAGS}"
-            "MYLDFLAGS="
-            "MYLIBS="
-            a
-        INSTALL_COMMAND
-            "${CMAKE_COMMAND}" -E make_directory "${LUA_EXTERNAL_INSTALL_DIR}/lib"
-            COMMAND "${CMAKE_COMMAND}" -E make_directory "${LUA_EXTERNAL_INCLUDE_DIR}"
-            COMMAND "${CMAKE_COMMAND}" -E copy_if_different "<SOURCE_DIR>/liblua.a" "${LUA_EXTERNAL_LIBRARY}"
-            COMMAND "${CMAKE_COMMAND}" -E copy_if_different
-                "<SOURCE_DIR>/lua.h"
-                "<SOURCE_DIR>/luaconf.h"
-                "<SOURCE_DIR>/lualib.h"
-                "<SOURCE_DIR>/lauxlib.h"
-                "${LUA_EXTERNAL_HPP}"
-                "${LUA_EXTERNAL_INCLUDE_DIR}"
+    set_target_properties(lua_onelua PROPERTIES
+        POSITION_INDEPENDENT_CODE TRUE
     )
 
-    add_library(Lua::Lua STATIC IMPORTED)
-    set_target_properties(Lua::Lua PROPERTIES
-        IMPORTED_LOCATION "${LUA_EXTERNAL_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${LUA_EXTERNAL_INCLUDE_DIR}"
-    )
-    add_dependencies(Lua::Lua lua_external)
-    set(LUA_INCLUDE_DIR "${LUA_EXTERNAL_INCLUDE_DIR}")
+    add_library(Lua::Lua ALIAS lua_onelua)
+    set(LUA_INCLUDE_DIR "${LUA_FETCH_INCLUDE_DIR}")
     set(LUA_LIBRARY Lua::Lua)
 endif()
 
--- cmake/Modules/FindMPG123.cmake
+++ cmake/Modules/FindMPG123.cmake
@@ -3,7 +3,7 @@
 # MPG123_INCLUDE_DIR
 # MPG123_LIBRARY
 
-include(ExternalProject)
+include(FetchContent)
 include(FindPackageHandleStandardArgs)
 
 find_path(MPG123_INCLUDE_DIR 
@@ -22,38 +22,38 @@ if(MPG123_INCLUDE_DIR AND MPG123_LIBRARY AND NOT TARGET MPG123::MPG123)
 endif()
 
 if(NOT TARGET MPG123::MPG123)
-    find_program(MAKE_EXECUTABLE NAMES gmake make REQUIRED)
+    set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build shared libraries" FORCE)
+    set(BUILD_PROGRAMS OFF CACHE BOOL "Build mpg123 programs" FORCE)
 
-    set(MPG123_EXTERNAL_PREFIX "${CMAKE_BINARY_DIR}/_deps/mpg123")
-    set(MPG123_EXTERNAL_INSTALL_DIR "${MPG123_EXTERNAL_PREFIX}/install")
-    set(MPG123_EXTERNAL_INCLUDE_DIR "${MPG123_EXTERNAL_INSTALL_DIR}/include")
-    set(MPG123_EXTERNAL_LIBRARY "${MPG123_EXTERNAL_INSTALL_DIR}/lib/libmpg123.a")
-
-    file(MAKE_DIRECTORY "${MPG123_EXTERNAL_INCLUDE_DIR}")
-
-    ExternalProject_Add(mpg123_external
-        PREFIX "${MPG123_EXTERNAL_PREFIX}"
+    FetchContent_Declare(mpg123
         GIT_REPOSITORY https://github.com/libsdl-org/mpg123.git
-        GIT_TAG master
+        GIT_TAG main
         GIT_SHALLOW TRUE
-        CONFIGURE_COMMAND
-            "<SOURCE_DIR>/configure"
-            "--prefix=<INSTALL_DIR>"
-            "--disable-shared"
-            "--enable-static"
-            "--disable-programs"
-        BUILD_COMMAND "${MAKE_EXECUTABLE}"
-        INSTALL_COMMAND "${MAKE_EXECUTABLE}" install
+        SOURCE_SUBDIR ports/cmake
     )
 
-    add_library(MPG123::MPG123 STATIC IMPORTED)
-    set_target_properties(MPG123::MPG123 PROPERTIES
-        IMPORTED_LOCATION "${MPG123_EXTERNAL_LIBRARY}"
-        INTERFACE_INCLUDE_DIRECTORIES "${MPG123_EXTERNAL_INCLUDE_DIR}"
-    )
-    add_dependencies(MPG123::MPG123 mpg123_external)
-    set(MPG123_INCLUDE_DIR "${MPG123_EXTERNAL_INCLUDE_DIR}")
-    set(MPG123_LIBRARY MPG123::MPG123)
+    FetchContent_MakeAvailable(mpg123)
+    set(MPG123_FETCH_INCLUDE_DIR "${mpg123_SOURCE_DIR}/src/include")
+
+    foreach(MPG123_TARGET_CANDIDATE
+            MPG123::libmpg123
+            mpg123::libmpg123
+            libmpg123
+            mpg123)
+        if(TARGET "${MPG123_TARGET_CANDIDATE}" AND NOT TARGET MPG123::MPG123)
+            get_target_property(MPG123_ALIASED_TARGET "${MPG123_TARGET_CANDIDATE}" ALIASED_TARGET)
+            if(MPG123_ALIASED_TARGET)
+                set(MPG123_TARGET "${MPG123_ALIASED_TARGET}")
+            else()
+                set(MPG123_TARGET "${MPG123_TARGET_CANDIDATE}")
+            endif()
+
+            target_include_directories("${MPG123_TARGET}" PUBLIC "$<BUILD_INTERFACE:${MPG123_FETCH_INCLUDE_DIR}>")
+            add_library(MPG123::MPG123 ALIAS "${MPG123_TARGET}")
+            set(MPG123_LIBRARY MPG123::MPG123)
+            set(MPG123_INCLUDE_DIR "${MPG123_FETCH_INCLUDE_DIR}")
+        endif()
+    endforeach()
 endif()
 
 if(TARGET MPG123::MPG123)
